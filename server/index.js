595eae6cf76a0a6e249287f2dd14900e3a1cf8a7
Krishna Shivhare <krishnashivhare197@gmail.com>
2026-06-27 10:47:55 +0530

    Fix formatting in CORS middleware configuration

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({ 
-    origin:["https://eduspark-1-rwfy.onrender.com","http://localhost:5173"]
+    origin:["https://eduspark-1-rwfy.onrender.com","http://localhost:5173"],
     credentials:true,
     methods: ["GET", "POST", "PUT" , "DELETE" , "OPTIONS"]
    }
