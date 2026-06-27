7ae5f964f922619818a7646c1813b79d6ae814a4
Krishna Shivhare <krishnashivhare197@gmail.com>
2026-06-27 10:43:31 +0530

    Refactor CORS settings for improved origin support
    Updated CORS configuration to allow multiple origins.

--- server/index.js
+++ server/index.js
@@ -13,18 +13,20 @@ import { stripeWebhook } from "./controllers/credits.controller.js";
 dotenv.config()
 const app = express()
 
+app.use(cors({ 
+    origin:["https://eduspark-1-rwfy.onrender.com","http://localhost:5173"]
+    credentials:true,
+    methods: ["GET", "POST", "PUT" , "DELETE" , "OPTIONS"]
+   }
+))
+
 app.post(
     "/api/credits/webhook",
     express.raw({type:"application/json"}),
     stripeWebhook
 );
 
-app.use(cors(
-   { origin:"http://localhost:5173",
-    credentials:true,
-    methods: ["GET", "POST", "PUT" , "DELETE" , "OPTIONS"]
-   }
-))
+
 
 app.use(express.json())
 app.use(cookieParser())
